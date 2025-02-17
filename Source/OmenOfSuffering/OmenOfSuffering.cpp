87363cbe98bdbb3f9facbaba59d386e446faad3a
Amadeusz <dluzniewskiamadeusz@gmail.com>
2025-02-17 22:07:51 +0100

    Removed Unreal Engine files from commit

---
+++ Source/OmenOfSuffering/OmenOfSuffering.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OmenOfSuffering.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OmenOfSuffering, "OmenOfSuffering" );
